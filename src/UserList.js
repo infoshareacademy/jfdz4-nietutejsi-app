88f10633c0ce7904b2bdbdd67e2089e8910d8701
Artur Falborski <falborski.artur@gmail.com>
2017-05-12 19:38:45 +0200

    changed component declaration formula

--- src/UserList.js
+++ src/UserList.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import './UserList.css'
 
-function UserList(props) {
-    return (
-        <table>
-            <tr>
-                <td>
-                    id
-                </td>
-                <td>
-                    Username
-                </td>
-                <td>
-                    First Name
-                </td>
-                <td>
-                    Last Name
-                </td>
-                <td>
-                    E-mail
-                </td>
-                <td>
-                    Last online
-                </td>
-            </tr>
-        </table>
-    )
+class UserList extends React.Component {
+    render() {
+        return (
+            <table>
+                <tr>
+                    <td>
+                        id
+                    </td>
+                    <td>
+                        Username
+                    </td>
+                    <td>
+                        First Name
+                    </td>
+                    <td>
+                        Last Name
+                    </td>
+                    <td>
+                        E-mail
+                    </td>
+                    <td>
+                        Last online
+                    </td>
+                </tr>
+            </table>
+        )
+    }
 }
 
 export default UserList;
