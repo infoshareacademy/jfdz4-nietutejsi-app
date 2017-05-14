277e628f671f06280adde9770dcca8f83bf9f2e5
Artur Falborski <falborski.artur@gmail.com>
2017-05-14 12:32:42 +0200

    removed react app intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  Grid
+} from 'react-bootstrap';
 import './App.css';
 import UserList from './UserList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <UserList/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Grid>
+    <UserList/>
+  </Grid>
+)
 
 export default App;
