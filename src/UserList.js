bd6003995d19fdd0a7bf94e499925837609cd152
Artur Falborski <falborski.artur@gmail.com>
2017-05-14 11:51:31 +0200

    fetched user.json file and mapped users to table

--- src/UserList.js
+++ src/UserList.js
@@ -2,9 +2,28 @@ import React from 'react';
 import './UserList.css'
 
 class UserList extends React.Component {
+    
+    constructor(props){
+        super(props)
+        
+        this.state = {
+            users: []
+        }
+
+        fetch(
+          process.env.PUBLIC_URL + '/data/users.json'
+        ).then(
+          response => response.json()
+        ).then(
+          users => this.setState({
+              users: users
+          })
+        )
+    }
     render() {
         return (
             <table>
+                <thead>
                 <tr>
                     <td>
                         id
@@ -22,9 +41,38 @@ class UserList extends React.Component {
                         E-mail
                     </td>
                     <td>
-                        Last online
+                        Last login
                     </td>
                 </tr>
+                </thead>
+                <tbody>
+                {
+                  this.state.users.map(
+                    user => (
+                      <tr key={user.id}>
+                          <td>
+                              {user.id}
+                          </td>
+                          <td>
+                              {user.username}
+                          </td>
+                          <td>
+                              {user.name}
+                          </td>
+                          <td>
+                              {user.surname}
+                          </td>
+                          <td>
+                              {user.email}
+                          </td>
+                          <td>
+                              {user.lastLogin}
+                          </td>
+                      </tr>
+                    )
+                  )
+                }
+                </tbody>
             </table>
         )
     }
