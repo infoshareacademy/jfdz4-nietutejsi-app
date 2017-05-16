dc015944f6525a283e608b8131fee93345b050a9
user <ravpaluch@gmail.com>
2017-05-16 21:29:58 +0200

    added Panel do App

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import Panel from './Panel';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Panel/>
-      </div>
-    );
-  }
-}
+const App = () =>
+<Grid>
+    <Panel/>
+</Grid>
 
 export default App;
