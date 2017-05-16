c1b3d4de489dcb466569f2b408b64c862b956e7a
user <ravpaluch@gmail.com>
2017-05-16 21:45:43 +0200

    removed {Component} from App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+
+import {Grid} from 'react-bootstrap'
 import './App.css';
 import Panel from './Panel';
 
-const App = () =>
+const App = () => (
 <Grid>
     <Panel/>
 </Grid>
+)
 
 export default App;
