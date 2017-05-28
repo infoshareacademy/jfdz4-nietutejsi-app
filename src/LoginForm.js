f88f29a3baa1c91bb6b089eb43935cba2aee8b11
agnieszka wieczerzyńska <anik@wp.pl>
2017-05-28 12:02:38 +0200

    formularz logowania e_mail

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -3,55 +3,80 @@
  */
 import React from 'react'
 import {
-Col,
-FormGroup,
-FormControl,
-ControlLabel,
-Form,
-Checkbox,
-Button
+    Col,
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    Form,
+    Checkbox,
+    Button
 } from 'react-bootstrap'
 
 class LoginForm extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            userName: '',
+            userPass: ''
+        }
+
+        this.handleUserName = event => {
+
+            this.setState({
+                userName: event.target.value
+            })
+
+        }
+
+    }
+
     render() {
         return (
-            <Form horizontal>
-                <FormGroup controlId="formHorizontalEmail">
-                    <Col componentClass={ControlLabel} sm={2}>
-                        Email
-                    </Col>
-                    <Col sm={10}>
-                        <FormControl type="email" placeholder="Email" />
-                    </Col>
-                </FormGroup>
+            <div>
+                <Form horizontal>
+                    <FormGroup controlId="formHorizontalEmail">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Email
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl
+                                value={this.state.userName}
+                                type="email"
+                                placeholder="Email"
+                                onChange={this.handleUserName}
+                            />
+                        </Col>
+                    </FormGroup>
 
-                <FormGroup controlId="formHorizontalPassword">
-                    <Col componentClass={ControlLabel} sm={2}>
-                        Hasło
-                    </Col>
-                    <Col sm={10}>
-                        <FormControl type="Haslo" placeholder="Haslo" />
-                    </Col>
-                </FormGroup>
+                    <FormGroup controlId="formHorizontalPassword">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Hasło
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl type="Haslo" placeholder="Haslo"/>
+                        </Col>
+                    </FormGroup>
 
-                <FormGroup>
-                    <Col smOffset={2} sm={10}>
-                        <Checkbox>Pamiętaj mnie</Checkbox>
-                    </Col>
-                </FormGroup>
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Checkbox>Pamiętaj mnie</Checkbox>
+                        </Col>
+                    </FormGroup>
 
-                <FormGroup>
-                    <Col smOffset={2} sm={10}>
-                        <Button type="submit">
-                            Zaloguj
-                        </Button>
-                    </Col>
-                </FormGroup>
-            </Form>
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Button type="submit">
+                                Zaloguj
+                            </Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+            </div>
 
         )
     }
 }
-
 
 export default LoginForm
