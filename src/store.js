c74f818455fad8c2e02fdd5f5324044b8bddf7a7
Artur Falborski <falborski.artur@gmail.com>
2017-06-11 12:44:38 +0200

    removed excessive content from store.js

--- src/store.js
+++ src/store.js
@@ -2,25 +2,9 @@ import { createStore } from 'redux'
 
 import reducer from './state/reducer'
 
-const usersKey = 'usersData'
-
-const persistedUsers = JSON.parse(localStorage.getItem(usersKey))
-
-const initialState = persistedUsers ? {
-  users: persistedUsers
-} : undefined
-
 const store = createStore(
   reducer,
-  initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-
-store.subscribe(
-  () => {
-    const stateToPersist = store.getState().users
-    localStorage.setItem(usersKey, JSON.stringify(stateToPersist))
-  }
 )
 
 export default store
