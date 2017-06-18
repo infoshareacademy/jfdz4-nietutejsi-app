00aa1de3ae0216837ae734490390cc98effc7059
Artur Falborski <falborski.artur@gmail.com>
2017-06-18 13:02:26 +0200

    change URL depending on navigation route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import {
-  Grid,
   Nav,
-  NavItem
+  NavItem,
+  Navbar
 } from 'react-bootstrap';
 import {
   BrowserRouter,
-  Route,
   Link
 } from 'react-router-dom'
 import './App.css';
 
 const App = () => (
-  <Grid>
-    <Nav bsStyle="pills">
-      <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
-      <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
-      <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
-    </Nav>
-  </Grid>
+  <div>
+    <BrowserRouter>
+    <Navbar>
+        <Nav pullLeft>
+          <img src="http://lorempizza.com/100/50"/>
+        </Nav>
+        <Nav>
+            <NavItem>
+              <Link to="/wishes">Życzenia</Link>
+            </NavItem>
+        </Nav>
+        <Nav pullRight>
+            <NavItem>
+              <Link to ="/profile">Mój profil</Link>
+            </NavItem>
+        </Nav>
+    </Navbar>
+      </BrowserRouter>
+    </div>
 )
 
 export default App;
