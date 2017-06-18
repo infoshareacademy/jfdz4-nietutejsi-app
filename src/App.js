823368bfb73a4f00011c3fb6ba877a1f1d6bb02a
Artur Falborski <falborski.artur@gmail.com>
2017-06-18 12:19:57 +0200

    add initial navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import {
-  Grid
+  Grid,
+  Nav,
+  NavItem
 } from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css';
 
 const App = () => (
   <Grid>
+    <Nav bsStyle="pills">
+      <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
+      <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
+      <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
+    </Nav>
   </Grid>
 )
 
