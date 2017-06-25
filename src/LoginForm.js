a5f95b3519b1454e804e13e04f9458ef4b64641e
agnieszka wieczerzyńska <anik@wp.pl>
2017-06-25 13:48:16 +0200

    firebase

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import './LoginForm.css';
+import firebase from 'firebase'
 import {
     Col,
     FormGroup,
@@ -32,7 +33,24 @@ class LoginForm extends React.Component {
         }
 
     }
+handleSubmit = event => {
 
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+        document.getElementById('quickstart-sign-in').disabled = false;
+        // [END_EXCLUDE]
+    });
+
+}
     render() {
         return (
             <div>
---
+++ src/SignUpForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoginForm.css';
+import firebase from 'firebase'
+import {
+    Col,
+    FormGroup,
+    FormControl,
+    ControlLabel,
+    Form,
+    Checkbox,
+    Button
+} from 'react-bootstrap';
