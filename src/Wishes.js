a3cbc59644af46608721035e8f0b459a9dc3c46d
Artur Falborski <falbor45@gmail.com>
2017-06-27 17:39:18 +0200

    fetch wishes from greetings.json

--- src/Wishes.js
+++ src/Wishes.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Wishes extends Component {
+  constructor(props) {
+    super(props)
+
+      this.state = {
+        greetings: []
+      }
+      fetch(
+          process.env.PUBLIC_URL + '/data/greetings.json'
+      ).then(
+          response => response.json()
+      ).then(
+          greetings => this.setState({
+              greetings: greetings
+          })
+      )
+  }
+
   render() {
     return (
       <div>
