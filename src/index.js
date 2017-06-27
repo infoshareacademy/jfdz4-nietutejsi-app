57498c523f7fe3f606a299b1838166e236ff973a
Artur Falborski <falbor45@gmail.com>
2017-06-27 17:30:54 +0200

    removed unnecessary stuff from App.js and renamed it to Wishes.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Wishes from './Wishes';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Wishes />,
   document.getElementById('root')
 );
