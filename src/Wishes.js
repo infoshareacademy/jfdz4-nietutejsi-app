babde719f483cb9a4341964527bd30572eeac713
Artur Falborski <falbor45@gmail.com>
2017-06-27 18:13:39 +0200

    indent lines in component's code

--- src/Wishes.js
+++ src/Wishes.js
@@ -1,75 +1,75 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class Wishes extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-      this.state = {
-        greetings: [],
-        searchValue: ''
-      }
-      fetch(
-          process.env.PUBLIC_URL + '/data/greetings.json'
-      ).then(
-          response => response.json()
-      ).then(
-          greetings => this.setState({
-              greetings: greetings
-          })
-      )
+        this.state = {
+            greetings: [],
+            searchValue: ''
+        }
+        fetch(
+            process.env.PUBLIC_URL + '/data/greetings.json'
+        ).then(
+            response => response.json()
+        ).then(
+            greetings => this.setState({
+                greetings: greetings
+            })
+        )
 
-      this.handleSearch = event => this.setState ({
-          searchValue: event.target.value
-      })
-  }
+        this.handleSearch = event => this.setState({
+            searchValue: event.target.value
+        })
+    }
 
-  render() {
-    return (
-      <div>
-        <input type="text" placeholder="Szukaj życzeń!" onChange={this.handleSearch}/>
-        <table>
-          <thead>
-          <tr>
-            <th>
-              Rodzaj
-            </th>
-            <th>
-              Treść
-            </th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-              this.state.greetings.filter(
-              wish => (
-              this.state.searchValue === '' ? true : [
-              wish.content,
-              wish.eventType
-              ].map(
-              wish => wish.toLowerCase()
-              ).some(searchValue => searchValue.includes(this.state.searchValue.toLowerCase()
-              )
-              )
-              )
-              ).map(
-                  wish => (
-                      <tr key={wish.id}>
-                        <td>
-                            {wish.eventType}
-                        </td>
-                        <td>
-                            {wish.content}
-                        </td>
-                      </tr>
-                  )
-              )
-          }
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+              <input type="text" placeholder="Szukaj życzeń!" onChange={this.handleSearch}/>
+              <table>
+                <thead>
+                <tr>
+                  <th>
+                    Rodzaj
+                  </th>
+                  <th>
+                    Treść
+                  </th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    this.state.greetings.filter(
+                        wish => (
+                            this.state.searchValue === '' ? true : [
+                                    wish.content,
+                                    wish.eventType
+                                ].map(
+                                    wish => wish.toLowerCase()
+                                ).some(searchValue => searchValue.includes(this.state.searchValue.toLowerCase()
+                                    )
+                                )
+                        )
+                    ).map(
+                        wish => (
+                            <tr key={wish.id}>
+                              <td>
+                                  {wish.eventType}
+                              </td>
+                              <td>
+                                  {wish.content}
+                              </td>
+                            </tr>
+                        )
+                    )
+                }
+                </tbody>
+              </table>
+            </div>
+        );
+    }
 }
 
 export default Wishes;
