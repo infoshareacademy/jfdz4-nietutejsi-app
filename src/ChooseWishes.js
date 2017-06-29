1665dfc7b8e37b214a5d485fa1ac75aa0dd00411
user <ravpaluch@gmail.com>
2017-06-29 18:24:15 +0200

    override modal css with custom one

--- src/ChooseWishes.js
+++ src/ChooseWishes.js
@@ -2,15 +2,14 @@
  * Created by user on 29.06.17.
  */
 import React from'react'
-import {Button, ButtonToolbar, Modal as BModal} from 'react-bootstrap'
+import {Grid, Table,Button, ButtonToolbar, Modal as BModal} from 'react-bootstrap'
+import './custom.css'
 
 
 export default class ChooseWishes extends React.Component {
 
     state = {show: false,
              wishes:[]
-
-
     }
 
     showModal =() => {
@@ -38,22 +37,43 @@ componentWillMount() {
         return (
 
             <ButtonToolbar>
-                <Button bsStyle="primary" onClick={this.showModal} style={{'display':'inline'}} >
+                <Button bsStyle="primary" onClick={this.showModal} >
                     Wybierz Życzenia
                 </Button>
 
-                <BModal
 
+                <BModal
                     show={this.state.show}
                     onHide={this.hideModal}
-                    dialogClassName="custom-modal"
-
-                >
+                    >
                     <BModal.Header closeButton>
                         <BModal.Title id="contained-modal-title-lg">{this.props.name} {this.props.surname}</BModal.Title>
                     </BModal.Header>
                     <BModal.Body>
+                            <div>
+                                <Table striped bordered condensed hover responsive>
+                                    <thead>
+                                    <tr>
 
+                                        <th>Rodzaj</th>
+                                        <th>Treść</th>
+
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {this.state.wishes.map(
+                                        wish => (
+                                            <tr key={wish.id}>
+                                                <td >{wish.eventType}</td>
+                                                <td>{wish.content}</td>
+
+                                            </tr>
+                                        )
+                                    )
+                                    }
+                                    </tbody>
+                                </Table>
+                            </div>
 
 
 
