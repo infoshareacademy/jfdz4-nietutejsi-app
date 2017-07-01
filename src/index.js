8dae84c2b201a94325da31f0f2fbb7437d734cc4
Artur Falborski <falborski.artur@gmail.com>
2017-07-01 15:49:00 +0200

    change App.js filename to AppView.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import AppView from './App';
+import AppView from './AppView';
 import './index.css';
 
 ReactDOM.render(
