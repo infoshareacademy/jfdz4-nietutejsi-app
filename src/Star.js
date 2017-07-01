a181cb466e62c0063b6c28d0b9c8609e2f688aa7
martaswiderska <martaaa.swiderska@gmail.com>
2017-07-01 14:06:40 +0200

    addition greetings

--- src/Star.js
+++ src/Star.js
@@ -2,25 +2,44 @@ import React from 'react';
 
 class Star extends React.Component {
     constructor(props) {
-    super(props)
+        super(props)
         this.state= {
             greetings: [],
-        star: 'white',
+            star: 'white',
             favoriteGreetings: []
         }
+
+        fetch(
+            process.env.PUBLIC_URL + '/data/greetings.json'
+        ).then(
+            response => response.json()
+        ).then(
+            greetings => this.setState({
+                greetings: greetings
+            })
+        )
+
         this.showStar= () => {this.setState({
             star: 'yellow'
         })}
     }
+
     render(){
         return(
-            <div>
-                <img  onClick={this.showStar}
-                      style={{"width": "40px"}}
-                      src={"/gwiazda.png"}
-                      alt="gwiazda"/>
+            <div>{
+                this.state.greetings.map(
+                    greeting => (
+
+                        <img  onClick={ () =>{this.showStar
+                this.setState({
+                favoriteGreetings: this.state.favoriteGreetings.filter(
+                  id => id !== greeting.id).concat(greeting.id)})}}
+
+                              style={{"width": "40px"}}
+                              src={"/gwiazda.png"}
+                              alt="gwiazda"/>))}
             </div>
         )
     }
-    }
-    export default Star
+}
+export default Star
