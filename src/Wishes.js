0feb09354338369a361bf42c393ba58ce0c34ddb
Marta Swiderska <martaaa.swiderska@gmail.com>
2017-07-01 18:48:19 +0200

    wishes search

--- src/Wishes.js
+++ src/Wishes.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import './Wishes.css';
 import Star from './Star'
+import {FormGroup, Navbar, Button, FormControl} from 'react-bootstrap';
 
 class Wishes extends Component {
     constructor(props) {
         super(props)
-            this.state = {
+        this.state = {
             greetings: [],
             searchValue: '',
             //     star: 'white'
@@ -18,67 +20,73 @@ class Wishes extends Component {
         ).then(
             greetings => this.setState({
                 greetings: greetings
-            },() => {
-            this.state.greetings.filter(
-                greating => this.state.favoriteGreetings.includes(greating.id)
-            )}))
+            }, () => {
+                this.state.greetings.filter(
+                    greating => this.state.favoriteGreetings.includes(greating.id)
+                )
+            }))
         this.handleSearch = event => this.setState({
             searchValue: event.target.value
-        })}
+        })
+    }
+
     render() {
         return (
             <div>
-              <input type="text" placeholder="Szukaj życzeń!" onChange={this.handleSearch}/>
-              <table>
-                <thead>
-                <tr>
-                  <th className="title1">
-                    Rodzaj
-                  </th>
-                  <th>
-                    Treść
-                  </th>
-                    <th>
-                        <p>Dodaj do ulubionych</p>
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    this.state.greetings.filter(
-                        wish => (
-                            this.state.searchValue === '' ? true : [
-                                    wish.content,
-                                    wish.eventType
-                                ].map(
-                                    wish => wish.toLowerCase()
-                                ).some(searchValue => searchValue.includes(this.state.searchValue.toLowerCase()
+                <div className="szukaj">
+                    <input className="szukaj-input" type="text" placeholder="Szukaj życzeń!"
+                           onChange={this.handleSearch}/>
+                </div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th className="title-rodzaj">
+                            Rodzaj
+                        </th>
+                        <th className="title-tresc">
+                            Treść
+                        </th>
+                        <th className="title-ulubione">
+                            <p>Ulubione</p>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.greetings.filter(
+                            wish => (
+                                this.state.searchValue === '' ? true : [
+                                        wish.content,
+                                        wish.eventType
+                                    ].map(
+                                        wish => wish.toLowerCase()
+                                    ).some(searchValue => searchValue.includes(this.state.searchValue.toLowerCase()
+                                        )
                                     )
-                                )
+                            )
+                        ).map(
+                            wish => (
+                                <tr key={wish.id}>
+                                    <td>
+                                        {wish.eventType}
+                                    </td>
+                                    <td className="wish-text">
+                                        {wish.content}
+                                    </td>
+                                    <td className="alignc">
+                                        <Star className="star"/>
+                                    </td>
+                                </tr>
+                            )
                         )
-                    ).map(
-                        wish => (
-                            <tr key={wish.id}>
-                              <td>
-                                  {wish.eventType}
-                              </td>
-                              <td>
-                                  {wish.content}
-                              </td>
-                                <td>
-                                    <Star/>
-
-
-                                </td>
-                            </tr>
-                        )
-                    )
-                }
-                </tbody>
-              </table>
+                    }
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
 
 export default Wishes;
+
+
